fe9a4307715b8d9b02c297434fb5f5c13ae51da0
Iulia Marin <julix.alna@yahoo.com>
2022-03-08 01:28:04 +0200

    restul de exercitii

--- laborator/content/reprezentare-numere/4-rotations/rotations.c
+++ laborator/content/reprezentare-numere/4-rotations/rotations.c
@@ -1,22 +1,43 @@
 #include <stdio.h>
 
 void rotate_left(int *number, int bits)
-{
-	/* TODO */
+{ 
+
+	printf("numar: %d \n", *number);
+	 int copie = *number;
+	*number = *number << bits;
+	printf("numar: %d \n", *number);
+	printf(" copie: %d \n", copie);
+	copie = copie >> (32-bits);
+	printf(" copie modificata: %d \n", copie);
+	*number = *number | copie;
+
 	(void) number;
 	(void) bits;
 }
 
 void rotate_right(int *number, int bits)
 {
-	/* TODO */
+	 int copie=*number;
+	*number = *number >> bits;
+	printf("numar: %d \n", *number);
+	printf(" copie: %d \n", copie);
+	copie = copie << (32-bits);
+	printf(" copie modificata: %d \n", copie);
+	*number = *number | copie;
+
 	(void) number;
 	(void) bits;
 }
 
 int main()
 {
-	/* TODO: Test functions */
+	int number,bits;
+	scanf("%d %d", &number , &bits);
+	rotate_left(&number,bits);
+	// rotate_right(&number,bits);
+	printf("%d",number);
+
 
 	return 0;
 }
