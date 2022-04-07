101bc22ead6ca74d6adf235d18354e39641b0552
Iulia Marin <julix.alna@yahoo.com>
2022-04-07 17:29:14 +0300

    Lab5
    Signed-off-by: Iulia Marin <julix.alna@yahoo.com>

--- laborator/content/introducere-asamblare/6-fibonacci/fibonacci.asm
+++ laborator/content/introducere-asamblare/6-fibonacci/fibonacci.asm
@@ -5,8 +5,20 @@ section .text
     extern printf
 
 main:
-    mov eax, 7       ; vrem sa aflam al N-lea numar; N = 7
-
-    ; TODO: calculati al N-lea numar fibonacci (f(0) = 0, f(1) = 1)
-
+    mov eax, 4       ; vrem sa aflam al N-lea numar; N = 7
+    mov ebx, 0
+    mov ecx, 1
+    mov edx, 0
+start:
+    cmp eax, 0
+    jne addone
+    je PRINTF32 `%d\n\x0`, ebx ; afiseaza numarul
     ret
+
+addone:
+    sub eax, 1  ;scadem pasul
+    mov edx, ecx    ;valoarea mare e pastrata
+    add ecx, ebx    ;valoarea mare e incementata cu cea precedenta
+    mov ebx, edx    ;valoarea mare devine cea mica
+    jmp start
+
