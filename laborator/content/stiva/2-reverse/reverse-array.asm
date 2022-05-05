9ac4c1ac2ad077c4415d65d084268de2ed856036
Iulia Marin <julix.alna@yahoo.com>
2022-05-05 15:35:04 +0300

    lab8
    Signed-off-by: Iulia Marin <julix.alna@yahoo.com>

--- laborator/content/stiva/2-reverse/reverse-array.asm
+++ laborator/content/stiva/2-reverse/reverse-array.asm
@@ -15,15 +15,41 @@ main:
 
     ; TODO push the elements of the array on the stack
     ; TODO retrieve the elements (pop) from the stack into the output array
+    mov ecx, ARRAY_LEN
+    sub ecx, 1
+
+punem:
+    cmp ecx, 0
+    jl out
+    push dword[input + ecx * 4]
+    sub ecx, 1
+    jmp punem
+
+out:
+    ; TODO retrieve the elements (pop) from the stack into the output array
+
+    xor ecx, ecx
+    mov ecx, ARRAY_LEN
+    sub ecx, 1
+
+scoatem:
+    cmp ecx, 0
+    jl gata
+    pop eax
+    mov dword[output + ecx * 4], eax
+    sub ecx, 1
+    jmp scoatem
+
+gata:
 
     PRINTF32 `Reversed array: \n\x0`
     xor ecx, ecx
+
 print_array:
     mov edx, [output + 4 * ecx]
     PRINTF32 `%d\n\x0`, edx
     inc ecx
     cmp ecx, ARRAY_LEN
     jb print_array
-
     xor eax, eax
     ret
